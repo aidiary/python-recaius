9d0c4d9a66684b6cb0e66f226d3e90ec7ad2fc71
aidiary <f2forest@gmail.com>
2016-04-02 16:57:33 +0900

    Add test_RecaiusTTS.py

---
+++ tests/test_RecaiusTTS.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import unittest
+from recaius.RecaiusTTS import RecaiusTTS, RecaiusTTSException
+
+class RecaiusTTSTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.rec = RecaiusTTS('YOUR_ID', 'YOUR_PASSWORD')
+
+    def setUp(self):
+        self.rec.reset()
+
+    def test_speaker(self):
+        self.rec.speak('サクラです。')
+        self.rec.speaker('hiroto').speak('ヒロトです。')
+
+    def test_emotion(self):
+        self.rec.speaker('sakura').emotion('happy', 100).speak('うれしいです。')
+        self.rec.speaker('sakura').emotion('angry', 100).speak('怒ってます。')
+        self.rec.speaker('sakura').emotion('sad', 100).speak('悲しいです。')
+
+    def test_speed(self):
+        self.rec.speaker('moe')
+        self.rec.speed(-10).speak('ゆっくりです。')
+        self.rec.speed(10).speak('早口です。')
+
+    def test_pitch(self):
+        self.rec.pitch(-10).speak('低い声です。')
+        self.rec.pitch(10).speak('高い声です。')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
